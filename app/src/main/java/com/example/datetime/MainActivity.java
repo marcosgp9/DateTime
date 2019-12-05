903937d5d63531816d7560334074acf187e5eb16
marna <marcosgalvez@gmail.com>
2019-12-05 16:52:14 +0100

    DateTime

---
+++ app/src/main/java/com/example/datetime/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.datetime;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void mostrarMensaje(View view){
+        Toast.makeText(this,"Estoy mamadisimo.",Toast.LENGTH_LONG).show();
+    }
+
+}
